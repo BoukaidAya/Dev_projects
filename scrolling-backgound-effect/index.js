999af9d24a502122aac576910bdf1785c58f8a8c
aboukaid <aboukaid@ayaline.com>
2025-11-09 17:29:06 +0100

    scorlling backgound effect

---
+++ scrolling-backgound-effect/index.js
@@ -0,0 +1,10 @@
+const BgImageEl = document.getElementById("bg-image");
+
+window.addEventListener("scroll" , () =>{
+    updateImage();
+});
+
+function updateImage(){
+    BgImageEl.style.opacity = 1 - window.pageYOffset /900;
+    BgImageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%"
+};
